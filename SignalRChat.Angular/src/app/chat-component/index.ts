6b2c84377c60469f220f25bc8807d7d2ef3c8447
Jeremy Little <muqluk@yahoo.com>
2020-09-15 20:15:56 -0500

    working angular sample as well now

--- SignalRChat.Angular/src/app/chat-component/index.ts
+++ SignalRChat.Angular/src/app/chat-component/index.ts
@@ -1,10 +1,47 @@
 import { Component } from "@angular/core";
 
+import { HubConnectionBuilder } from '@microsoft/signalr';
+
 @Component({
   selector: 'chat-component',
   styleUrls: ['chat.scss'],
   templateUrl: 'chat.html',
 })
 export class Chat {
+  public _nick: string;
+  set nick(v) {
+    this._nick = v;
+  }
+
+  private HubConnectionBuilder = HubConnectionBuilder;
+  private connection;
+  public message: string;
+  public messages = [];
+
+  ngOnInit() {
+    this.nick = window.prompt('Your name:', 'John');
+    this.connection = new HubConnectionBuilder()
+      .withUrl("http://localhost:5000/chathub")
+      .build();
+
+    this.connection
+      .start()
+      .then(() => console.log('Connection started!'))
+      .catch(err => console.log('Error while establishing connection :('));
+
+    this.connection.on('sendToAll', this.recMessage);
+  }
+
+  sendMessage = () => {
+    this.connection
+      .invoke('sendToAll', this._nick, this.message)
+      .catch(err => console.error(err));
+    this.message = '';
+  };
+
+  recMessage = (nick, receivedMessage) => {
+    const text = `${nick}: ${receivedMessage}`;
+    const messages = this.messages.push(text);
+  }
 
 }
